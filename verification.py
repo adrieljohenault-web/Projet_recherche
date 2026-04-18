b7ee5e55b059d3a6c8fac4b189cc66d11151ac69
adrieljohenault-web <adrieljohenault@gmail.com>
2026-04-18 20:18:38 +0200

    Création du fichier contenant l'agorithme de vérification de la fonction de transfert.

---
+++ verification.py
@@ -0,0 +1,2 @@
+# Ce programme a pour but d'évaluer l'efficacité et la précision de notre fonction de transfert réalisée sans apprentissage automatique. Ses résultats seront par la suite utilisés pour être comparés avec ceux de la fonction de transfert réalisée à l'aide de cette approche.
+
