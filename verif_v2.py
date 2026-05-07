196547afa7dd93929b291007250a833221e4a158
clementcg01 <c.cottegrubis@gmail.com>
2026-05-08 00:19:13 +0200

    en train de construire verif 2

---
+++ verif_v2.py
@@ -0,0 +1,127 @@
+# l'objectif de ce fichier est de fournir une comparaison entre les fonctions toruver par la fonction de transfert et les données réelles
+#import
+
+import os 
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+
+from variables_globales import *
+
+
+# ---------- Importation des données de sortie mesurées ---------
+# verif_k = [YYYY, MM, DD, hh, mm, ss, h[m], Hm0[m], Hs[m], Tm[s], Tp[s]] 
+
+with open(os.path.join(path, "Capteurs_pression", "S1_recup_capteur-haut_2012-12-13_2013-03-12_waveStats_filt_h01.1.dat"), 'r'
+) as file :
+    lines = file.readlines()
+for i in range(len(lines)):
+    lines[i] = lines[i].split()
+
+verif_1 = np.array(lines)
+verif_1 = verif_1[1:]
+
+with open(os.path.join(path, "Capteurs_pression", "S2_recup_capteur-bas_2012-12-13_2013-03-12_waveStats_filt_h01.1.dat"), 'r') as file :
+    lines = file.readlines()
+for i in range(len(lines)):
+    lines[i] = lines[i].split()
+
+verif_2 = np.array(lines)
+verif_2 = verif_2[7:-5]
+
+with open(os.path.join(path, "Capteurs_pression", "S3_capteur_offshore_2012-12-11_2013-03-14_waveStats_filt_h01.1.dat"), 'r') as file :
+    lines = file.readlines()
+for i in range(len(lines)):
+    lines[i] = lines[i].split()
+
+verif_3 = np.array(lines)
+verif_3 = verif_3[300:-311]
+
+
+#on va mettre dans un format plus simple à comprendre 
+# ( pour v3, on garde la valeur de dpt car elle va nous permettre de calculer le coefiscient des marées )
+
+def format_sonde(verif: np.ndarray) -> np.ndarray:
+    dts = np.array([
+        datetime.datetime(int(row[0]), int(row[1]), int(row[2]),
+                 int(row[3]), int(row[4]))
+        for row in verif
+    ], dtype=object)
+
+    Hs  = verif[:, 8].astype(float)
+    Tp  = verif[:, 10].astype(float)
+    dpt = verif[:, 6].astype(float)
+
+    return np.column_stack([dts, Hs, Tp, dpt])
+
+v1 = format_sonde(verif_1)
+v2 = format_sonde(verif_2)
+v3 = format_sonde(verif_3)
+
+# obtention des coefiscient de la marée en fonction de la date
+
+def get_extremum(hauteurs: list):
+    hauteurs.sort()
+    ind_5percent = int(np.ceil(len(hauteurs) * 0.05))
+    high = np.mean(hauteurs[-ind_5percent:])
+    low = np.mean(hauteurs[:ind_5percent])
+    return high, low
+
+def get_coef_marree(v_sonde:np.array):
+    dpt = v_sonde[:,-1]
+    dpt_sorted = sorted(dpt)
+
+    high,low = get_extremum(dpt_sorted)
+
+    arr_coef = np.array([[date, 0] for date in v_sonde[:,0]])
+
+    for i in range(len(v_sonde)):
+        coef = min(1,max(( dpt[i] - low ) / ( high - low ),0))
+        arr_coef[i][1] = coef
+    
+    return arr_coef
+
+v_marree = get_coef_marree(v3)
+
+# on intersect les données pour qu'elles soient compatibles sur les dates
+
+def synchroniser(*arrays: np.ndarray) -> tuple:
+    """
+    Ne garde que les lignes dont la date (colonne 0) est présente
+    dans tous les arrays passés en argument.
+    """
+    # Ensemble des dates communes à tous les arrays
+    dates_communes = set(arrays[0][:, 0])
+    for arr in arrays[1:]:
+        dates_communes &= set(arr[:, 0])
+
+    # Filtrage de chaque array
+    arrays_sync = tuple(
+        arr[np.array([dt in dates_communes for dt in arr[:, 0]])]
+        for arr in arrays
+    )
+    return arrays_sync
+
+v1, v2, v3, v_marree = synchroniser(v1, v2, v3, v_marree)
+
+# ---------- Importation des données de mesurées au large ---------
+
+with open(os.path.join(path, "Vagues_forcage", "Waves_resourcecode_138311.csv"), 'r') as file :
+    lines = file.readlines()
+for i in range(len(lines)):
+    lines[i] = lines[i].split()
+
+vin0 = []
+
+for donnees in verif_in:
+    donnees[0] = donnees[0].split('-')
+
+    donnees[1] = donnees[1].split(',')
+    donnees[1][0] = donnees[1][0].split(':')
+    donnees[1][0].pop(2)
+    vin0.append([donnees[0][j] for j in range(3)]+[donnees[1][0][j] for j in range(2)]+[donnees[1][j] for j in range(1, 14)])
+
+
+verif_in = np.array(lines[1:])
+
+print(verif_in[0])
